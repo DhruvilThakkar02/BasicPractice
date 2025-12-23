39b0394ae23729394e81e2d1068aa79741bc5fab
Dhruvil <tbs.dhruvil@gmail.com>
2025-12-23 11:38:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicUnderstadng
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
